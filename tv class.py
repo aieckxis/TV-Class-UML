2d2258dfbaf4d208a07f9542defc4862a4c1477d
aieckxis <aleckxiskate@gmail.com>
2023-05-13 15:08:58 +0800

    methods complete but error (invalid syntax line 28)

--- tv class.py
+++ tv class.py
@@ -7,21 +7,49 @@ class TV:
     channel = 1
     volume = 1
     tv_on = False
+
     # Constructor
     def __init__(self, channel = 1, volume = 1):
         self.channel = channel
         self.volume = volume
+
     # Methods
     # Turn on the TV
+    def turnOn(self):
+        self.tv_on = True
     # Turn off the TV
-    # Return the channel for the TV
+    def turnOff(self):
+        self.tv_off = True
+    # Return the channel for the TV (1 to 120)
+    def getChannel(self):
+        return self.channel
     # Set a new channel for the TV
-    # Get the volume level for the TV
+    def setChannel(self):
+        if self.channel 1 <= channel <= 120:
+            self.channel = channel
+    # Get the volume level for the TV (1 to 7)
+    def getVolume(self):
+        return self.volume
     # Set a new volume level for the TV
+    def setVolume(self):
+        if self.volume 1 <= volume <= 7:
+            self.volume = volume
     # Increase the channel number by 1
+    def channelUp(self):
+        if self.tv_on and self.channel < 120:
+            channel += 1
     # Decrease the channel number by 1
+    def channelDown(self):
+    if self.tv_on and self.channel > 1:
+        channel -= 1
     # Increase the volume level by 1
+    def volumeUp(self):
+        if self.tv_on and self.volume < 7:
+            volume += 1
     # Decrease the volume level by 1
+    def volumeDown(self):
+        if self.tv_on and self.volume > 1:
+            volume -= 1
 # Define the TestTV function
 # Create a TV object with channel 30 and volume level 3
 # Turn on the TV1
