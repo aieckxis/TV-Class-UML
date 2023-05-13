f33ac9ec6cb4e87f3a844f0c494c312894eb45c4
aieckxis <aleckxiskate@gmail.com>
2023-05-13 13:47:07 +0800

    pseudocode

---
+++ tv class.py
@@ -0,0 +1,15 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Define the TV class
+# Attributes
+# Constructor
+# Methods
+# Define the TestTV function
+# Create a TV object with channel 30 and volume level 3
+# Turn on the TV1
+# Print the current channel and volume level of the TV
+# Create another TV object with channel 3 and volume
+# Turn on the TV2
+# Print the current channel and volume level of the TV
+# Call the TestTV function to run the tests
