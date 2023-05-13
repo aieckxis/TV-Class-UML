4591fc7f54aa0c68aebfec22e45532a8c4a771f1
aieckxis <aleckxiskate@gmail.com>
2023-05-13 15:53:22 +0800

    working on tv1

--- tv class.py
+++ tv class.py
@@ -37,21 +37,30 @@ class TV:
     # Increase the channel number by 1
     def channelUp(self):
         if self.tv_on and self.channel < 120:
-            channel += 1
+            self.channel += 1
     # Decrease the channel number by 1
     def channelDown(self):
         if self.tv_on and self.channel > 1:
-            channel -= 1
+            self.channel -= 1
     # Increase the volume level by 1
     def volumeUp(self):
         if self.tv_on and self.volume < 7:
-            volume += 1
+            self.volume += 1
     # Decrease the volume level by 1
     def volumeDown(self):
         if self.tv_on and self.volume > 1:
-            volume -= 1
+            self.volume -= 1
 # Define the TestTV function
+def TestTV():
+    # Create a TV object with channel 30 and volume level 3 (output)
+    tv1 = TV(30, 3)
+    # Turn on the TV1
+    tv1.turnOn()
+    # Print the current channel and volume level of the TV
+    print("tv1 channel is", tv1.getChannel(), "and the volume level is", tv1.getVolume())
+# Create another TV object with channel 3 and volume level 2 (output)
 # Create another TV object with channel 3 and volume level 2 (output)
 # Turn on the TV2
 # Print the current channel and volume level of the TV
 # Call the TestTV function to run the tests
+TestTV()
